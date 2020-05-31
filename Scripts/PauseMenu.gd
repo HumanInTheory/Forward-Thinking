6778623ba6058047534b40cc947bf8cf9f05930d
Fuzzyzilla <fuzzyzilla01@gmail.com>
2020-05-31 13:20:20 -0700

    Fixed HTML5 export not supporting get_tree().exit, mutable audio, configured HTML5 and Winx86/64 exports

--- Scripts/PauseMenu.gd
+++ Scripts/PauseMenu.gd
@@ -6,6 +6,8 @@ export var initial_volume = -10
 
 func _ready():
 	_on_VolumeSlider_value_changed(initial_volume)
+	$"CenterContainer/HBoxContainer/VBoxContainer/Quit".visible = !OS.has_feature("HTML5")
+		
 
 func _on_Resume_pressed():
 	get_parent().get_parent().resume()
@@ -17,7 +19,10 @@ func _on_Sensitivity_value_changed(value):
 	get_parent().get_parent().sensitivity = value
 
 func _on_VolumeSlider_value_changed(value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
+	if value == -30:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -100)
+	else:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
 
 func _on_Fullscreen_toggled(button_pressed):
 	OS.set_window_fullscreen(button_pressed)
